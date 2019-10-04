e92ee9ef744fa63103cd51733d7f05f305451de0
Kamil Tomaszewski <kamil.tomaszewski@sony.com>
2019-10-04 11:42:51 +0200

    Add kwarg to disable renumbering endpoints

--- adafruit_usb_descriptor/util.py
+++ adafruit_usb_descriptor/util.py
@@ -22,7 +22,7 @@
 
 from . import standard
 
-def join_interfaces(*args):
+def join_interfaces(args, *, renumber_endpoints=True):
     """Renumbers interfaces and endpoints so they are compatible.
 
        ``args`` is any number of interface sequences (usually lists with
@@ -40,23 +40,12 @@ def join_interfaces(*args):
             for subdescriptor in interface.subdescriptors:
                 if (subdescriptor.bDescriptorType ==
                         standard.EndpointDescriptor.bDescriptorType):
-                    subdescriptor.bEndpointAddress += base_endpoint_number
-                    endpoint_address = subdescriptor.bEndpointAddress & 0xf
-                    max_endpoint_address = max(max_endpoint_address,
-                                               endpoint_address)
+                    if renumber_endpoints:
+                        subdescriptor.bEndpointAddress += base_endpoint_number
+                        endpoint_address = subdescriptor.bEndpointAddress & 0xf
+                        max_endpoint_address = max(max_endpoint_address,
+                                                endpoint_address)
+                    elif subdescriptor.bEndpointAddress == 0:
+                        raise ValueError('Endpoint address must not be 0')
             base_endpoint_number = max_endpoint_address + 1
-    return interfaces
-
-def renumbers_interfaces(*args):
-    """Renumbers interfaces so they are compatible.
-
-       ``args`` is any number of interface sequences (usually lists with
-       `InterfaceDescriptor` s inside them). Interfaces within a sequence
-       should be numbered beginning at 0x0."""
-    interfaces = []
-    for interface_set in args:
-        base_interface_number = len(interfaces)
-        for i, interface in enumerate(interface_set):
-            interfaces.append(interface)
-            interface.bInterfaceNumber = interfaces.index(interface)
     return interfaces
